e2568df03540f2a1b07f4b2a491cb4fea841c343
sakib shabir <52796746+sakib1913@users.noreply.github.com>
2020-06-22 15:19:06 +0530

    Add files via upload

---
+++ text to speech/test.py
@@ -0,0 +1,12 @@
+from gtts import gTTS 
+
+import os
+fh = open("test.txt","r")
+myText = fh.read().replace("\n"," ")
+
+language= 'en'
+
+output = gTTS(text=myText,lang=language ,slow=False)
+fh.close()
+output.save("output.mp3")
+
