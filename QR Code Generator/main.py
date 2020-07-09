d1cce6057bc542c8c1ce0d26db152a61417b705c
sakib shabir <52796746+sakib1913@users.noreply.github.com>
2020-07-09 12:38:02 +0530

    Add files via upload

---
+++ QR Code Generator/main.py
@@ -0,0 +1,11 @@
+import qrcode
+qr=qrcode.QRCode(
+version=1,
+box_size=10,
+border=5
+)
+data="My name is sakib|i m Programer|Web devloper"
+qr.add_data(data)
+qr.make(fit=True)
+img=qr.make_image(fill="black",back_color="white")
+img.save("1.png")
